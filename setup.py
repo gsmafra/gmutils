6257865279997d8df2fc17b26b3058f4fff37017
gsmafra <gsenamafra@gmail.com>
2015-08-10 14:40:08 +0200

    add setup

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='gmutils',
+    version='0.1.0',
+    author='Gustavo Sena Mafra',
+    author_email='gsenamafra@gmail.com',
+    description='Utils',
+    license='MIT',
+    packages=find_packages(),
+    url='https://github.com/gsmafra/gmutils',
+    install_requires=['numpy'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'License :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Scientific/Engineering :: Machine Learning',
+        ]
+    )
