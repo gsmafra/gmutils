fedb27b74b61e2f0361cbb14dcf2802b9d90cec0
Sena Mafra_Gustavo prest Duong Quang-Khanh-Ngoc  04/09/15 (email=gustavo.senamafra@technicolor.com) <senamafrag@renndlxrdl3057.rennes.eu.thmulti.com>
2015-08-10 14:29:15 +0200

    First commit

---
+++ listutils.py
@@ -0,0 +1,23 @@
+def most_common(l):
+
+	return max(set(l), key=l.count)
+
+def unique_elements(l):
+
+	return list(set(l))
+
+def indexes_matching(l, item):
+
+	return [i for i,x in enumerate(l) if x == item]
+
+def sub_list(l, index):
+
+	return [l[i] for i in list(index)]
+
+def join_lists(l):
+
+	return [item for sublist in l for item in sublist]
+	
+def replicate_list(l, n):
+
+	return join_lists([[item]*n for item in l])
